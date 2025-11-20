b9e183e04d11623605672a480c333a9e469fb50f
minsungJun <asrjatk@gmail.com>
2025-11-20 20:09:05 +0900

    init2

--- Source/CatchMe/CatchMe.Build.cs
+++ Source/CatchMe/CatchMe.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 
@@ -9,5 +9,12 @@ public class CatchMe : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-	}
+
+        PublicIncludePaths.AddRange(new string[]
+        {
+            "CatchMe", // 이게 없으면 헤더파일 인클루드시 경로를 찾지 못함.
+		});
+    }
+
+
 }
